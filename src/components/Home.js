d6e7d4882cb3ae953b068185b74095388db8f3ec
LouryNay <123560671+LouryNay@users.noreply.github.com>
2024-10-11 20:57:25 +0200

    home debug sans style

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,153 +3,153 @@ import xml2js from 'xml2js';
 import xmlFile from './../cv.xml';
 
 const Home = ({ language }) => {
-    const [data, setData] = useState(null);
-    const [timelineItems, setTimelineItems] = useState([]);
-    const [competences, setCompetences] = useState([]);
+  const [data, setData] = useState(null);
+  const [timelineItems, setTimelineItems] = useState([]);
+  const [competences, setCompetences] = useState([]);
 
-    const extractAndSortTimelineItems = useCallback((cvData) => {
-        const diplomas = cvData.formation[0].diplome.map(diplome => ({
-            type: 'formation',
-            title: diplome.int[0][language][0],
-            date: diplome.date[0],
-            lieu: diplome.lieu[0][language][0],
-            sortDate: parseDate(diplome.date[0])
-        }));
+  const extractAndSortTimelineItems = useCallback((cvData) => {
+    const diplomas = cvData.formation[0].diplome.map(diplome => ({
+      type: 'formation',
+      title: diplome.int[0][language][0],
+      date: diplome.date[0],
+      lieu: diplome.lieu[0][language][0],
+      sortDate: parseDate(diplome.date[0])
+    }));
 
-        const experiences = cvData.experiences[0].exp.map(exp => ({
-            type: 'experience',
-            title: exp.int[0][language][0],
-            date: exp.date[0],
-            lieu: exp.lieu[0][language][0],
-            sortDate: parseDate(exp.date[0])
-        }));
+    const experiences = cvData.experiences[0].exp.map(exp => ({
+      type: 'experience',
+      title: exp.int[0][language][0],
+      date: exp.date[0],
+      lieu: exp.lieu[0][language][0],
+      sortDate: parseDate(exp.date[0])
+    }));
 
-        const allItems = [...diplomas, ...experiences].sort((a, b) => b.sortDate - a.sortDate);
-        setTimelineItems(allItems);
-    }, [language]); // Ajoutez 'language' si cela change les résultats
+    const allItems = [...diplomas, ...experiences].sort((a, b) => a.sortDate - b.sortDate);
+    setTimelineItems(allItems);
+  }, [language]); // Ajoutez 'language' si cela change les résultats
 
-    const parseDate = (dateStr) => {
-        const [startDate] = dateStr.split(' - ');
-        const [month, year] = startDate.split('/');
-        return parseInt(year + month.padStart(2, '0'), 10);
+  const parseDate = (dateStr) => {
+    const [startDate] = dateStr.split(' - ');
+    const [month, year] = startDate.split('/');
+    return parseInt(year + month.padStart(2, '0'), 10);
+  };
+
+  const extractCompetences = useCallback((cvData) => {
+    const competencesData = [];
+
+    // Extraire les langues
+    const langues = {
+      category: language === 'fr' ? 'Langues' : 'Languages',
+      skills: cvData.langues[0].lang.map(lang => ({
+        name: lang.l[0][language][0],
+        level: lang.niveau[0][language][0]
+      })),
+      link: '#languages'
     };
 
-    const extractCompetences = useCallback((cvData) => {
-        const competencesData = [];
+    // Ajouter les langues regroupées à la liste des compétences
+    competencesData.push(langues);
 
-        // Extraire les langues
-        const langues = cvData.langues[0].lang.map(lang => ({
-            category: language === 'fr' ? 'Langues' : 'Languages',
-            skills: [
-                {
-                    name: lang.l[0][language][0],
-                    level: lang.niveau[0][language][0]
-                }
-            ],
-            link: '#languages'
-        }));
 
-        competencesData.push(...langues);
+    // Extraire les autres compétences
+    const categories = cvData.competences[0].categorie.map(categorie => {
+      // Récupération et tri des compétences de la balise comp
+      const compSkills = (categorie.comp || [])
+        .map(comp => ({
+          name: comp.name[0][language][0],
+          level: comp.niv[0]
+        }))
+        .sort((a, b) => b.level - a.level); // Trier les compétences par niveau décroissant
 
-        // Extraire les autres compétences
-        const categories = cvData.competences[0].categorie.map(categorie => {
-          // Récupération et tri des compétences de la balise comp
-          const compSkills = (categorie.comp || [])
-              .map(comp => ({
-                  name: comp.name[0][language][0],
-                  level: comp.niv[0]
-              }))
-              .sort((a, b) => b.level - a.level); // Trier les compétences par niveau décroissant
-      
-          // Récupération et tri des cours de la balise cours
-          const coursSkills = (categorie.cours || [])
-              .map(cours => ({
-                  name: cours.name[0][language][0],
-                  level: cours.niv[0]
-              }))
-              .sort((a, b) => b.level - a.level); // Trier les cours par niveau décroissant
-      
-          // Concaténer les compétences et les cours, les compétences en premier
-          const skills = [...compSkills, ...coursSkills];
-      
-          return {
-              category: categorie.ti[0][language][0],
-              skills: skills,
-              link: `#${categorie.ti[0][language][0].toLowerCase().replace(/\s+/g, '-')}`
-          };
-      });
-      
+      // Récupération et tri des cours de la balise cours
+      const coursSkills = (categorie.cours || [])
+        .map(cours => ({
+          name: cours.name[0][language][0],
+          level: cours.niv[0]
+        }))
+        .sort((a, b) => b.level - a.level); // Trier les cours par niveau décroissant
 
-        competencesData.push(...categories);
-        setCompetences(competencesData);
-    }, [language]); // Ajoutez 'language' si cela change les résultats
+      // Concaténer les compétences et les cours, les compétences en premier
+      const skills = [...compSkills, ...coursSkills];
 
-    useEffect(() => {
-        fetch(xmlFile)
-            .then(response => response.text())
-            .then(str => xml2js.parseStringPromise(str))
-            .then(result => {
-                console.log('Parsed XML:', result);
-                if (result && result.cv) {
-                    setData(result.cv);
-                    extractAndSortTimelineItems(result.cv);
-                    extractCompetences(result.cv);
-                }
-            })
-            .catch(error => console.error('Erreur lors du chargement du fichier XML:', error));
+      return {
+        category: categorie.ti[0][language][0],
+        skills: skills,
+        link: `#${categorie.ti[0][language][0].toLowerCase().replace(/\s+/g, '-')}`
+      };
+    });
 
-    }, [language, extractAndSortTimelineItems, extractCompetences]); // Ajoutez les fonctions ici
 
-    if (!data) return <div>Loading...</div>;
+    competencesData.push(...categories);
+    setCompetences(competencesData);
+  }, [language]); // Ajoutez 'language' si cela change les résultats
 
-    return (
-        <div className="home">
-            {/* Section Présentation */}
-            <section className="presentation">
-                <div className="presentation-container">
-                    <img className="profile-photo" src="" alt="Profile" />
-                    <p className="nom">{data.profil[0].nom}</p>
-                    <p className="titre">{language === 'fr' ? data.profil[0].titre[0].fr[0] : data.profil[0].titre[0].en[0]}</p>
-                    <p className="presentation-description">{language === 'fr' ? data.profil[0].description[0].fr[0] : data.profil[0].description[0].en[0]}</p>
-                </div>
-            </section>
+  useEffect(() => {
+    fetch(xmlFile)
+      .then(response => response.text())
+      .then(str => xml2js.parseStringPromise(str))
+      .then(result => {
+        console.log('Parsed XML:', result);
+        if (result && result.cv) {
+          setData(result.cv);
+          extractAndSortTimelineItems(result.cv);
+          extractCompetences(result.cv);
+        }
+      })
+      .catch(error => console.error('Erreur lors du chargement du fichier XML:', error));
 
-            {/* Section Frise Chronologique */}
-            <section className="timeline">
-                <h3>{language === 'fr' ? 'Frise Chronologique' : 'Timeline'}</h3>
-                <div className="timeline-content">
-                    {timelineItems.map((item, index) => (
-                        <div className="timeline-item" key={index}>
-                            <h4>
-                                {item.title} ({item.date})
-                            </h4>
-                            <p>{item.lieu}</p>
-                        </div>
-                    ))}
-                </div>
-            </section>
+  }, [language, extractAndSortTimelineItems, extractCompetences]); // Ajoutez les fonctions ici
 
-            {/* Section Compétences */}
-            <section className="competence-carousel">
-                <h3>{language === 'fr' ? 'Compétences' : 'Skills'}</h3>
-                <div className="competence-content">
-                    {competences.map((comp, index) => (
-                        <div className="competence-item" key={index}>
-                            <h4>{comp.category}</h4>
-                            <ul>
-                                {comp.skills.map((skill, idx) => (
-                                    <li key={idx}>{skill.name}</li>
-                                ))}
-                            </ul>
-                            <a href={comp.link} className="details-button">
-                                {language === 'fr' ? 'Plus de détails' : 'More details'}
-                            </a>
-                        </div>
-                    ))}
-                </div>
-            </section>
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <div className="home">
+      {/* Section Présentation */}
+      <section className="presentation">
+        <div className="presentation-container">
+          <img className="profile-photo" src="" alt="Profile" />
+          <p className="nom">{data.profil[0].nom}</p>
+          <p className="titre">{language === 'fr' ? data.profil[0].titre[0].fr[0] : data.profil[0].titre[0].en[0]}</p>
+          <p className="presentation-description">{language === 'fr' ? data.profil[0].description[0].fr[0] : data.profil[0].description[0].en[0]}</p>
         </div>
-    );
+      </section>
+
+      {/* Section Frise Chronologique */}
+      <section className="timeline">
+        <h3>{language === 'fr' ? 'Frise Chronologique' : 'Timeline'}</h3>
+        <div className="timeline-content">
+          {timelineItems.map((item, index) => (
+            <div className="timeline-item" key={index}>
+              <h4>
+                {item.title} ({item.date})
+              </h4>
+              <p>{item.lieu}</p>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      {/* Section Compétences */}
+      <section className="competence-carousel">
+        <h3>{language === 'fr' ? 'Compétences' : 'Skills'}</h3>
+        <div className="competence-content">
+          {competences.map((comp, index) => (
+            <div className="competence-item" key={index}>
+              <h4>{comp.category}</h4>
+              <ul>
+                {comp.skills.map((skill, idx) => (
+                  <li key={idx}>{skill.name}</li>
+                ))}
+              </ul>
+              <a href={comp.link} className="details-button">
+                {language === 'fr' ? 'Plus de détails' : 'More details'}
+              </a>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
