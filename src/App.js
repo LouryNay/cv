eca28cd2a5f5f8f74bf4448518ef8e5e76d199fd
LouryNay <123560671+LouryNay@users.noreply.github.com>
2024-10-14 14:29:45 +0200

    routage

--- src/App.js
+++ src/App.js
@@ -11,42 +11,42 @@ import Banner from './components/Banner';
 import './App.css';
 
 const App = () => {
-    const [language, setLanguage] = useState('fr');
-    const [cvData, setCvData] = useState(null);
+  const [language, setLanguage] = useState('fr');
+  const [cvData, setCvData] = useState(null);
 
-    const changeLanguage = (lang) => {
-        setLanguage(lang);
-    };
+  const changeLanguage = (lang) => {
+    setLanguage(lang);
+  };
 
-    useEffect(() => {
-        fetch(xmlFile)
-            .then(response => response.text())
-            .then(str => xml2js.parseStringPromise(str))
-            .then(result => {
-                console.log('Parsed XML:', result);
-                if (result && result.cv) {
-                    setCvData(result.cv);
-                }
-            })
-            .catch(error => console.error('Erreur lors du chargement du fichier XML:', error));
-    }, []);
+  useEffect(() => {
+    fetch(xmlFile)
+      .then(response => response.text())
+      .then(str => xml2js.parseStringPromise(str))
+      .then(result => {
+        console.log('Parsed XML:', result);
+        if (result && result.cv) {
+          setCvData(result.cv);
+        }
+      })
+      .catch(error => console.error('Erreur lors du chargement du fichier XML:', error));
+  }, []);
 
-    if (!cvData) return <div>Loading...</div>;
+  if (!cvData) return <div>Loading...</div>;
 
-    return (
-        <Router>
-            <div className="App">
-                <Banner language={language} changeLanguage={changeLanguage} />
-                <Routes>
-                    <Route path="/cv" element={<Home language={language} cvData={cvData} />} />
-                    <Route path="/cv/competences" element={<Competences language={language} cvData={cvData} />} />
-                    <Route path="/cv/formations" element={<Formations language={language} cvData={cvData} />} />
-                    <Route path="/cv/experiences" element={<Experiences language={language} cvData={cvData} />} />
-                    <Route path="/cv/autres" element={<Autres language={language} cvData={cvData} />} />
-                </Routes>
-            </div>
-        </Router>
-    );
-};
+  return (
+    <Router basename="/cv>">
+      <div className="App">
+        <Banner language={language} changeLanguage={changeLanguage} />
+        <Routes>
+          <Route path="/cv" element={<Home language={language} cvData={cvData} />} />
+          <Route path="/cv/competences" element={<Competences language={language} cvData={cvData} />} />
+          <Route path="/cv/formations" element={<Formations language={language} cvData={cvData} />} />
+          <Route path="/cv/experiences" element={<Experiences language={language} cvData={cvData} />} />
+          <Route path="/cv/autres" element={<Autres language={language} cvData={cvData} />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
 
 export default App;
