f8ab57b4c1767e7bae0d66491898cc4eea857fe5
LouryNay <123560671+LouryNay@users.noreply.github.com>
2024-10-16 22:16:06 +0200

    design lien compétences

--- src/components/Home.js
+++ src/components/Home.js
@@ -165,7 +165,7 @@ const Home = ({ language, cvData }) => {
                     </li>
                   ))}
                 </ul>
-                <a href={/competence/competence.link} className="details-button">
+                <a href={`/competences${comp.link}`} className="details-button">
                   {language === 'fr' ? 'Plus de détails' : 'More details'}
                 </a>
               </div>
